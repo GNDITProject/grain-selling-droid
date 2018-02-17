a07dc7e6dceca6c45ee0ccb13ee0ceb51b304d31
juniorjainsahab <p.jain3322@gmail.com>
2018-02-17 18:39:39 +0530

    adding signup and login activity

---
+++ app/src/main/java/com/prayasj/gndit/LoginActivity.java
@@ -0,0 +1,17 @@
+package com.prayasj.gndit;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by prayas on 17/2/18.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/com/prayasj/gndit/SignupActivity.java
@@ -0,0 +1,40 @@
+package com.prayasj.gndit;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+/**
+ * Created by prayas on 17/2/18.
+ */
+
+public class SignupActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+
+        View submitButton = findViewById(R.id.submitButton);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(SignupActivity.this,"hello",Toast.LENGTH_LONG).show();
+            }
+        });
+
+        TextView loginHere = findViewById(R.id.loginhere);
+        loginHere.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent loginIntent = new Intent(SignupActivity.this,LoginActivity.class);
+                SignupActivity.this.startActivity(loginIntent);
+            }
+        });
+    }
+}
