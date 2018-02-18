12f5e2aa626aa3f88ae835ba3dd94a027a93da37
juniorjainsahab <p.jain3322@gmail.com>
2018-02-18 18:14:36 +0530

    adding views in login activity and updating alignments in signup activity

--- app/src/main/java/com/prayasj/gndit/LoginActivity.java
+++ app/src/main/java/com/prayasj/gndit/LoginActivity.java
@@ -1,13 +1,24 @@
 package com.prayasj.gndit;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+      View signUpHere = findViewById(R.id.signup_here);
+      signUpHere.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+          Intent signUpIntent = new Intent(LoginActivity.this, SignupActivity.class);
+          LoginActivity.this.startActivity(signUpIntent);
+        }
+      });
     }
 }
